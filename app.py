f21ec86446eb017d91303f6070689168da0f64f1
Arquimedes Quintero <aruki@Arquimedess-MacBook-Pro.local>
2021-04-01 12:17:15 -0500

    porganized code in functions to make the code reusable

--- app.py
+++ app.py
@@ -7,17 +7,83 @@ from game import game
 import traceback
 import time
 
-# Driver configuration variables
-DOWNLOAD_FOLDER = './pdf/'
-DRIVER_FOLDER = '/Users/aruki/code/chromedriver'
-RESULTS_URL = 'https://fedebeis.com.pa/boxscore-juvenil-2020/'
-CSV_NAME = 'results_minor_league_2020.csv'
+
+# navigate the website to gather the game match links
+
+
+def getMatchLinks(webdriver):
+
+    # find the button for next pagination
+    next_button = driver.find_element_by_xpath(
+        "//*[@id='tie-block_2186']/div/div[1]/div/div/ul/li[2]/a")
+    # results = driver.find_elements_by_xpath("//li/h3/a")
+    # print('cantidad de resultados: {}'.format(len(results)))
+    partidos = []
+    loop = True
+    cont = 1
+
+    # loop the results
+    while loop:
+        wait = WebDriverWait(driver, 10)
+        next_button = wait.until(
+            EC.element_to_be_clickable((By.CLASS_NAME, 'next-posts')))
+
+        # next_button = driver.find_element_by_class_name('next-posts')
+        loop = not ('pagination-disabled' in next_button.get_attribute('class'))
+        #print(next_button.get_attribute('class') + ' | ' + str(loop))
+
+        results = driver.find_elements_by_xpath("//li/h3/a")
+        print(' {} results in page {} !'.format(str(len(results)), cont))
+
+        for result in results:
+            partido = game(result.text, result.get_attribute('href'))
+            partidos.append(partido)
+
+        if loop:
+            next_button.click()
+            # wait for the transition animation to finish loading results
+            time.sleep(6)
+            driver.implicitly_wait(6)
+
+        cont += 1
+
+    return partidos
+
+
+def downloadGameResults(webdriver, partidos):
+
+    print('Games to download: {}'.format(len(partidos)))
+    cont = 1
+    total = len(partidos)
+
+    # we loop through the gathered download links
+    for partido in partidos:
+        print('Downloading game result {}/{} {}'.format(str(cont),
+                                                        str(total), partido.titulo))
+        try:
+            cont += 1
+            driver.get(partido.link)
+            time.sleep(2)
+            # if not ('P.Pospuesto' in partido.titulo):
+
+            download = driver.find_element_by_class_name('s_pdf_download_link')
+            partido.download_link = download.get_attribute('href')
+            driver.get(download.get_attribute('href'))
+            partido.downloaded = True
+        except:
+            partido.download_link = ''
+            partido.downloaded = False
+            print('Encountered an issue downloading results for {}'.format(
+                partido.titulo))
+            # traceback.print_exc()
+
+    return partidos
 
 # dump the list of games and download links to a csv file
 
 
 def make_csv_file(results, file=CSV_NAME, echo=False):
-    with open(file, 'a') as f:
+    with open(file, 'w') as f:
         headers = 'match, result_link, download_link, downloaded\n'
         if echo:
             print(headers + '\n')
@@ -30,77 +96,42 @@ def make_csv_file(results, file=CSV_NAME, echo=False):
             f.write(line)
 
 
-# chrome options for download
-prefs = {
-    "download.default_directory": DOWNLOAD_FOLDER,
-    "download.prompt_for_download": False,
-    "download.directory_upgrade": True,
-    "plugins.always_open_pdf_externally": True,
-}
-chrome_options = Options()
-chrome_options.add_experimental_option('prefs', prefs)
+# returns the configured chrome web driver (the download folder is not working properly. Downloads to default)
 
-driver = webdriver.Chrome(DRIVER_FOLDER, options=chrome_options)
-driver.get(RESULTS_URL)
 
-# find the button for next pagination
-next_button = driver.find_element_by_xpath(
-    "//*[@id='tie-block_2186']/div/div[1]/div/div/ul/li[2]/a")
-# results = driver.find_elements_by_xpath("//li/h3/a")
-# print('cantidad de resultados: {}'.format(len(results)))
-partidos = []
-loop = True
-cont = 1
+def getChromeDriver(driver_location, download_folder):
+    # chrome options for download
+    prefs = {
+        "download.default_directory": download_folder,
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True,
+        "plugins.always_open_pdf_externally": True,
+    }
+    chrome_options = Options()
+    chrome_options.add_experimental_option('prefs', prefs)
 
-# loop the results
-while loop:
-    wait = WebDriverWait(driver, 10)
-    next_button = wait.until(
-        EC.element_to_be_clickable((By.CLASS_NAME, 'next-posts')))
+    driver = webdriver.Chrome(driver_location, options=chrome_options)
+    return driver
 
-    # next_button = driver.find_element_by_class_name('next-posts')
-    loop = not ('pagination-disabled' in next_button.get_attribute('class'))
-    #print(next_button.get_attribute('class') + ' | ' + str(loop))
 
-    results = driver.find_elements_by_xpath("//li/h3/a")
-    print(' {} results in page {} !'.format(str(len(results)), cont))
+if __name__ == '__main__':
+    # Driver configuration variables
+    DOWNLOAD_FOLDER = './pdf/'
+    DRIVER_FOLDER = '/Users/aruki/code/chromedriver'
+    RESULTS_URL = 'https://fedebeis.com.pa/boxscore-juvenil-2020/'
+    CSV_NAME = 'results_minor_league_2020.csv'
 
-    for result in results:
-        partido = game(result.text, result.get_attribute('href'))
-        partidos.append(partido)
+    driver = getChromeDriver(DRIVER_FOLDER, DOWNLOAD_FOLDER)
 
-    if loop:
-        next_button.click()
-        # wait for the transition animation to finish loading results
-        time.sleep(6)
-        driver.implicitly_wait(6)
+    driver.get(RESULTS_URL)
 
-    cont += 1
+    # we get the game links
+    partidos = getMatchLinks(driver)
 
-print('Games to download: {}'.format(len(partidos)))
-cont = 1
-total = len(partidos)
+    # download the pdf files
+    partidos = downloadGameResults(driver, partidos)
 
-# we loop through the gathered download links
-for partido in partidos:
-    print('Downloading game result {}/{} {}'.format(str(cont),
-          str(total), partido.titulo))
-    try:
-        cont += 1
-        driver.get(partido.link)
-        time.sleep(2)
-        # if not ('P.Pospuesto' in partido.titulo):
+    # make a csv file with the gathered links
+    make_csv_file(partidos, 'results_minor_league_2020.csv', True)
 
-        download = driver.find_element_by_class_name('s_pdf_download_link')
-        partido.download_link = download.get_attribute('href')
-        driver.get(download.get_attribute('href'))
-        partido.downloaded = True
-    except:
-        partido.download_link = ''
-        partido.downloaded = False
-        print('Encountered an issue downloading results for {}'.format(partido.titulo))
-        # traceback.print_exc()
-
-make_csv_file(partidos, 'results_minor_league_2020.csv', True)
-
-driver.quit()
+    driver.quit()
